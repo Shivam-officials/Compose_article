d58affc297cfa11231db50ac3256251863a53a68
shivam <suryabhardwdajofficial@gmail.com>
2023-08-15 15:27:46 +0530

    refractor the code

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -5,15 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -32,44 +31,62 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Homepage()
+                    ArticleApp()
                 }
             }
         }
     }
 }
 
+
+/*
+ * composable funtion for calling the ArticleCard
+ */
 @Composable
-fun Homepage(modifier: Modifier = Modifier) {
-    val image = painterResource(id = R.drawable.bg_compose_background)
+fun ArticleApp() {
+    ArticleCard(
+        title = stringResource(id = R.string.title),
+        imagePainter = painterResource(id = R.drawable.bg_compose_background),
+        shortDesc = stringResource(id = R.string.compose_short_desc),
+        longDesc = stringResource(id = R.string.compose_long_desc)
+    )
+}
+
+//articleCard Compose for Article Structure
+@Composable
+fun ArticleCard(
+    title: String,
+    imagePainter: Painter,
+    shortDesc: String,
+    longDesc: String,
+    modifier: Modifier = Modifier
+) {
 
     Column(modifier = modifier) {
         Image(
-            painter = image,
+            painter = imagePainter,
             contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth()
         )
 
         Text(
-            text = stringResource(R.string.title),
+            text = title,
             fontSize = 24.sp,
             modifier = Modifier
                 .padding(16.dp)
         )
 
         Text(
-            text = stringResource(R.string.compose_short_desc),
+            text = shortDesc,
             textAlign = TextAlign.Justify,
             modifier = modifier
                 .padding(start = 16.dp, end = 16.dp)
         )
 
         Text(
-            text = stringResource(R.string.compose_long_desc),
+            text = longDesc,
             textAlign = TextAlign.Justify,
             modifier = modifier
-                .padding( 16.dp)
+                .padding(16.dp)
         )
 
     }
@@ -78,6 +95,6 @@ fun Homepage(modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun HomepagePreview() {
-    Homepage()
+    ArticleApp()
 
 }
